15965c42bc2a0081120d28e9459fd77e78136368
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2025-12-13 16:19:19 -0300

    buttons icons

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -14,22 +14,22 @@ export default function Buttons({ onAdd, onCategories, onPreviousMonth, onNextMo
     <Container>
       <div className="rounded m-2 d-flex justify-content-center">
 
-        <Button variant="secondary" className="m-2" onClick={onPreviousMonth}>
+        <Button variant="secondary" className="m-2" onClick={onPreviousMonth} title="Mês Anterior">
           <MdOutlineKeyboardDoubleArrowLeft size={26} className="mb-1" />
           <span className="d-none d-md-inline">Mês Anterior</span>
         </Button>
 
-        <Button variant="warning" className="m-2" onClick={onCategories}>
+        <Button variant="warning" className="m-2" onClick={onCategories} title="Categorias">
           <MdList size={26} className="mb-1" />
           <span className="d-none d-md-inline">Categorias</span>
         </Button>
 
-        <Button variant="danger" className="m-2" onClick={onAdd}>
+        <Button variant="danger" className="m-2" onClick={onAdd} title="Nova Entrada">
           <MdOutlineAdd size={26} className="mb-1" />
           <span className="d-none d-md-inline">Nova Entrada</span>
         </Button>
 
-        <Button variant="secondary" className="m-2" onClick={onNextMonth}>
+        <Button variant="secondary" className="m-2" onClick={onNextMonth} title="Próximo Mês">
           <MdOutlineKeyboardDoubleArrowRight size={26} className="mb-1" />
           <span className="d-none d-md-inline">Próximo Mês</span>
         </Button>
