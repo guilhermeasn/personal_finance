a5fea056d232aa3ceb2b9996320b0691b2af8ce0
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2025-12-13 16:18:25 -0300

    buttons icons

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,5 +1,5 @@
 import { Button, Container } from "react-bootstrap";
-import { MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdList, MdOutlineAdd, MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 export type ButtonProps = {
   onPreviousMonth?: () => void;
@@ -13,10 +13,26 @@ export default function Buttons({ onAdd, onCategories, onPreviousMonth, onNextMo
   return (
     <Container>
       <div className="rounded m-2 d-flex justify-content-center">
-        <Button variant="secondary" className="m-2" onClick={onPreviousMonth}><MdOutlineKeyboardDoubleArrowLeft size={26} className="mb-1" />Mês Anterior</Button>
-        <Button variant="warning" className="m-2" onClick={onCategories}>Categorias</Button>
-        <Button variant="danger" className="m-2" onClick={onAdd}>Nova Entrada</Button>
-        <Button variant="secondary" className="m-2" onClick={onNextMonth}>Próximo Mês<MdOutlineKeyboardDoubleArrowRight size={26} className="mb-1" /></Button>
+
+        <Button variant="secondary" className="m-2" onClick={onPreviousMonth}>
+          <MdOutlineKeyboardDoubleArrowLeft size={26} className="mb-1" />
+          <span className="d-none d-md-inline">Mês Anterior</span>
+        </Button>
+
+        <Button variant="warning" className="m-2" onClick={onCategories}>
+          <MdList size={26} className="mb-1" />
+          <span className="d-none d-md-inline">Categorias</span>
+        </Button>
+
+        <Button variant="danger" className="m-2" onClick={onAdd}>
+          <MdOutlineAdd size={26} className="mb-1" />
+          <span className="d-none d-md-inline">Nova Entrada</span>
+        </Button>
+
+        <Button variant="secondary" className="m-2" onClick={onNextMonth}>
+          <MdOutlineKeyboardDoubleArrowRight size={26} className="mb-1" />
+          <span className="d-none d-md-inline">Próximo Mês</span>
+        </Button>
       </div>
     </Container>
   )
